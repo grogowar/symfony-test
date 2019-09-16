c0040019b2ef5262f6cfd7b5309a9309ef5c2204
shuranov-a <shuranov-a@speechpro.com>
2019-09-16 23:01:32 +0300

    added easycorp/easy-deploy-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
 ];
